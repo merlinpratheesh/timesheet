88dfaa85733ac3b474cb6a784a34bfdfd989d21c
merlinpratheesh <merlinpratheesh@gmail.com>
2021-08-08 00:17:43 +0530

    styles Added onetap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { AppComponent } from './app.component';
 import { OfflineScreenComponent } from './offline-screen/offline-screen.component';
 
 const routes: Routes = [
-  
+  { path: 'app', component: AppComponent },
   { path: 'admin', component: AdminComponent },
   { path: 'offline', component: OfflineScreenComponent },
 
-  { path: '',   redirectTo: '/admin', pathMatch: 'full' },
+  { path: '',   redirectTo: 'admin', pathMatch: 'full' },
 
-  { path: '**', redirectTo: '/admin', pathMatch: 'full'},
+  { path: '**', redirectTo: 'admin', pathMatch: 'full'},
 ];
 
 @NgModule({
